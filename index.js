4de1aae6147329fb8758816c16e7e93708ba578d
Tahmid Islam <tahmid231@gmail.com>
2021-10-24 13:14:44 +0600

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(express().json());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
